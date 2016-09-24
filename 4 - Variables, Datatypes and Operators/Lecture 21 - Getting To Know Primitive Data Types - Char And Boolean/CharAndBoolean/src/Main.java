d42fdc608c1a8eeb0784a34c4e37f3793581d57e
gabriel301 <gabriel301@github.com>
2016-09-24 18:48:11 -0300

    Lecture 21

---
+++ 4 - Variables, Datatypes and Operators/Lecture 21 - Getting To Know Primitive Data Types - Char And Boolean/CharAndBoolean/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Augus on 24/09/2016.
+ */
+public class Main {
+    public static void main(String[] args){
+        //Char - 16 bits (2 bytes) - Because Unicode
+        char myChar = 'D';
+
+        //Boolean - 8 bits (1 byte)
+        boolean myBoolean = true;
+    }
+}
