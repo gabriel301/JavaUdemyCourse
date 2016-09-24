6640e94ec8dd3c7ceb459f55ca4fa364e181806c
gabriel301 <gabriel301@github.com>
2016-09-24 11:01:47 -0300

    Hello World

---
+++ 3 - First Steps/HelloWorld/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Augus on 24/09/2016.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Hello Gabriel!");
+    }
+}
