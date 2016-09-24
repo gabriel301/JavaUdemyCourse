0fa09b9482ef08c55721961689a754ae9b3cf7d9
gabriel301 <gabriel301@github.com>
2016-09-24 12:17:25 -0300

    Lecture 18 - What Are Variables

---
+++ 4 - Variables, Datatypes and Operators/Lecture 18 - What are variables/src/Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Augus on 24/09/2016.
+ */
+public class Hello {
+    public static void main(String[] args){
+        //int - primitive type(32 bits - 4 bytes)
+        int myFirstNumber = (10 + 5) + (2*10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber*2;
+        int myTotal = myFirstNumber+mySecondNumber+myThirdNumber;
+        int myTotal2 = 1000 - myTotal;
+        System.out.println(myFirstNumber);
+        System.out.println(myTotal);
+        System.out.println(myTotal2);
+
+    }
+}
