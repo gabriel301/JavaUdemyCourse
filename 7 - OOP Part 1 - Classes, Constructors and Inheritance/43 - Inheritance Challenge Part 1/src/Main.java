be8dbc77ad552f7a66c4a7ee9fe1e2e7824a78ec
gabriel301 <gabriel301@github.com>
2016-11-16 10:11:44 -0200

    Inheritance Challenge

--- 7 - OOP Part 1 - Classes, Constructors and Inheritance/43 - Inheritance Challenge Part 1/src/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by g.ferreira.oliveira on 14/11/2016.
- */
-//All classes inherits from the class java.lang.Object automatically
-public class Main {
-    public static void main(String[] args){
-
-    }
-}
